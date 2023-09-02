5064561f622eb12bc956f5b787fd477004c46bd2
Belamiri Zakarya <43291919+kakaa2993@users.noreply.github.com>
2023-09-02 09:43:18 +0100

    Update main.py
    Add type annotation for more clear code and readability

--- main.py
+++ main.py
@@ -4,38 +4,39 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 import csv
+from typing import List
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 app.secret_key = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"  # put your secret key here
 Bootstrap(app=app)
 
 
 class Form(FlaskForm):
-    cafe_name = StringField(label="Cafe Name", validators=[DataRequired()])
-    location = StringField(label="Cafe Location on Google Maps(URL)",
+    cafe_name: StringField = StringField(label="Cafe Name", validators=[DataRequired()])
+    location: StringField = StringField(label="Cafe Location on Google Maps(URL)",
                            validators=[DataRequired(), URL(message="Invalid URL")])
-    open = StringField(label="Opening Time e.g. 8AM ",
+    open: StringField = StringField(label="Opening Time e.g. 8AM ",
                        validators=[DataRequired()])
-    close = StringField(label="Closing Time e.g. 5:30PM ",
+    close: StringField = StringField(label="Closing Time e.g. 5:30PM ",
                         validators=[DataRequired()])
-    coffee_rating = SelectField(label="Coffee Rating",
+    coffee_rating: SelectField = SelectField(label="Coffee Rating",
                                 choices=["☕", "☕☕",  "☕☕☕",  "☕☕☕☕", "☕☕☕☕☕"],
                                 validators=[])
-    wifi_rating = SelectField(label="Wifi Strength Rating ",
+    wifi_rating: SelectField = SelectField(label="Wifi Strength Rating ",
                               choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"])
-    power_sockets = SelectField(label="Power Socket Availability",
+    power_sockets: SelectField = SelectField(label="Power Socket Availability",
                                 choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"])
-    submit = SubmitField(label="Submit")
+    submit: SubmitField = SubmitField(label="Submit")
 
 
 @app.route("/")
-def home():
+def home() -> str:
     return render_template("index.html")
 
 
 @app.route("/cafes")
-def cafes():
-    list_rows = []
+def cafes() -> str:
+    list_rows: List = []
     with open(file="cafe-data.csv", encoding="utf-8", newline='') as file:
         csv_file = csv.reader(file)
         for row in csv_file:
@@ -43,7 +44,7 @@ def cafes():
     return render_template("cafes.html", rows=list_rows)
 
 
-def add_data_to_database(detail):
+def add_data_to_database(detail) -> None:
     with open("cafe-data.csv", "a", encoding="utf-8", newline='') as csv_file:
         csv_file.write(",".join(detail))
 
@@ -52,7 +53,7 @@ def add_data_to_database(detail):
 def add():
     order_form = Form()
     if order_form.validate_on_submit():
-        data = [f"\n{order_form.cafe_name.data}", order_form.location.data, order_form.open.data, order_form.close.data, order_form.coffee_rating.data, order_form.wifi_rating.data, order_form.power_sockets.data]
+        data: List[str | None] = [f"\n{order_form.cafe_name.data}", order_form.location.data, order_form.open.data, order_form.close.data, order_form.coffee_rating.data, order_form.wifi_rating.data, order_form.power_sockets.data]
         add_data_to_database(data)
         return redirect(url_for('cafes'))
     return render_template("add.html", form=order_form)
